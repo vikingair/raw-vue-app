85c66fedabf7238ec77bc78635b6a6aae9c195c5
Viktor Luft <viktor.luft@freiheit.com>
2022-11-16 14:18:16 +0100

    Fix lint issue

--- src/components/ArticleList.test.ts
+++ src/components/ArticleList.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it, vi } from "vitest";
-import { fireEvent, render } from "@testing-library/vue";
+import { render } from "@testing-library/vue";
 import { createPinia } from "pinia";
 import ArticleList from "./ArticleList.vue";
 import { useStore } from "@/store";
